7aaad727cee35a0981fd19af9c08c8d6631d6926
KARAN K K <69041780+KKK2504@users.noreply.github.com>
2024-04-30 18:01:37 +0530

    Add files via upload

---
+++ Models/PaymentDetailDbContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace PaymentAPI.Models;
+
+public partial class PaymentDetailDbContext : DbContext
+{
+  
+
+    public PaymentDetailDbContext(DbContextOptions<PaymentDetailDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<PaymentDetail> PaymentDetails { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<PaymentDetail>(entity =>
+        {
+            entity.Property(e => e.CardNumber).HasMaxLength(16);
+            entity.Property(e => e.CardOwnerName).HasMaxLength(100);
+            entity.Property(e => e.ExpirationDate).HasMaxLength(5);
+            entity.Property(e => e.SecurityCode).HasMaxLength(3);
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__User__3213E83FC943CA8E");
+
+            entity.ToTable("User");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.CompanyId).HasColumnName("companyId");
+            entity.Property(e => e.CompanyName)
+                .HasMaxLength(255)
+                .HasColumnName("companyName");
+            entity.Property(e => e.CustomerName)
+                .HasMaxLength(255)
+                .HasColumnName("customerName");
+            entity.Property(e => e.Password)
+                .HasMaxLength(255)
+                .HasColumnName("password");
+            entity.Property(e => e.PolicyDetails).HasColumnName("policyDetails");
+            entity.Property(e => e.Username)
+                .HasMaxLength(255)
+                .HasColumnName("username");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PaymentAPI.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string CustomerName { get; set; } = null!;
+
+    public string CompanyName { get; set; } = null!;
+
+    public int CompanyId { get; set; }
+
+    public string PolicyDetails { get; set; } = null!;
+}
